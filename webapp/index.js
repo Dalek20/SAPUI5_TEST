a72fbb102e466432c413b3ba3575df36b5a6fdc2
Dalek20 <mpichunan@gmail.com>
2020-05-22 02:32:32 +0000

    5.3, aprendiendo control

--- webapp/index.js
+++ webapp/index.js
@@ -1,5 +1,5 @@
 sap.ui.define(["sap/m/Text"],function(Text){
-	/*eslint-disable no-alert*/
-	alert("Wena los k, asiendo el sap");
-	/*eslint-enable no-alert*/
+	new Text({
+		text:"hola hola"
+	}).placeAt("content");
 });
